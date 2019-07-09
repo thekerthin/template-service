109f6f208e896a37f3ed7a46aef8a5f23b8e7b3c
sebastiandelaroche <sebastian.delaroche.dev@gmail.com>
2019-07-09 18:53:20 -0500

    Put the EmitEvent decorator at CQRS npm package

--- src/domain/services/template-domain.services.ts
+++ src/domain/services/template-domain.services.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { TemplateDomainEntity } from '../entities/template-domain.entity';
-import { EmitEvent } from '../../shared/decorator';
 import { TemplateRepository } from '../../database/repositories/template.repository';
 
 @Injectable()
 export class TemplateDomainService {
-  constructor(private readonly repository: TemplateRepository) { }
 
-  // TODO: evaluate if we can emit an event with an annotation - check this whether works ;)
+  constructor(
+    private readonly repository: TemplateRepository
+  ) { }
+
+  // TODO: implement EmitEvent from cqrs module
   create() {
     // ... business rule
     // ... emit event
     // ... response (it depends of the logic) return;
   }
 
-  @EmitEvent({})
   async findAll(data: any): Promise<TemplateDomainEntity[]> {
     return this.repository.find();
   }
+
 }
