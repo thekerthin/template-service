9f5eee652ddaf1ad47bf0a26a5a878c42bfd845b
Sebastian De La Roche <sebastian.delaroche.dev@gmail.com>
2019-09-02 20:51:29 -0500

    changes

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ dotenvSafe.config();
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@kerthin/logger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new Logger(),
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
